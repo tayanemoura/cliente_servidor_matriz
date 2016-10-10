c2042c9b07a883d4d386de09f0b1820acba32474
tayanemoura <tayanemoura@id.uff.br>
2016-10-10 07:34:49 -0300

    fix server.c

--- client.c
+++ client.c
@@ -17,12 +17,22 @@ int main(void) {
 	//PF_INET = IPv4
 	sock = socket(PF_INET, SOCK_STREAM, 0);	
 
+	if(sock<0){
+		perror("Erro na criação do socket");
+	}
+
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(3000);
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)); 
+	
+	if(connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr))<0){
+		perror("Problema na conexão");
+	} 
+	else{
+		printf("Conectado\n");
+	}
 
 	recv(sock, recvBuffer, 1024, 0);
 
--- server.c
+++ server.c
@@ -19,23 +19,38 @@ int main(void) {
 	//PF_INET = IPv4
 	sock = socket(PF_INET, SOCK_STREAM, 0);	
 
+	if(sock<0){
+		perror("Erro na criação do socket");
+	}
+
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(3000);
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	
 
-	bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)); 
+	if(bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr))<0){
+		perror("bind - error");
+		exit(1);
+	} 
 
-	listen(sock, 10); 
-
-	while (1){
-
-		connection = accept(sock, (struct sockaddr *) &client_addr, sizeof(client_addr));
-		strcpy(buffer, "Hello World\n");
-		//int send(int sockfd, const void *msg, int len, int flags); 
-		send(connection, buffer, 13, 0);
-		close(connection);
+	if(listen(sock, 10) != 0){
+		perror("listen - error");
+		exit(2);
 	}
+
+	printf("%s\n","Esperando conexões...");
+
+	//while (1){
+	socklen_t client_size = sizeof(client_addr);
+	connection = accept(sock, (struct sockaddr *) &client_addr, &client_size);
+	//printf("%s\n","Received request...");
+	strcpy(buffer, "Hello World\n");
+	//int send(int sockfd, const void *msg, int len, int flags); 
+	send(connection, buffer, 13, 0);
+	close(connection);
+	//}
 
 	close(sock);
 }
