114807e2cf7663dd428e042801347dcc16777d07
tayanemoura <tayanemoura@id.uff.br>
2016-10-17 12:05:02 -0200

    apagando prints

--- client.py
+++ client.py
@@ -7,7 +7,6 @@ from datetime import *
 a = datetime.now()
 timestamp = a.strftime('%H%M%S%f')
 matrix_file_name = "matriz" + timestamp + ".txt"
-print matrix_file_name
 
 def create_matrix(matrix_size):
   
@@ -15,23 +14,17 @@ def create_matrix(matrix_size):
   for i in range(matrix_size):
   	matrix.append([0]*matrix_size)
 
-  print matrix
-
   matrix_file = open(matrix_file_name, 'a+')
   for i in range(0, matrix_size):
       for j in range(0, matrix_size):
           number = random.randint(0, 100)
           matrix[i][j] = number
-          print(str(number) + "\t")
           matrix_file.write((str(number) + "\t"))
-      print()
       matrix_file.write("\n")
-  print()
   matrix_file.close()
-  print matrix
   return matrix
 
-def print_matrix(matrix):
+def print_matrix(matrix, matrix_size):
   for i in range(0, matrix_size):
       for j in range(0, matrix_size):
           print(str(matrix[i][j]) + "\t")
@@ -47,9 +40,14 @@ sock.connect(server_addr)
 # tamanho da matriz
 m = sys.argv[1]
 
-m1 = create_matrix(int(m))
-m2 = create_matrix(int(m))
-#print m
+matrix_size = int(m)
+
+m1 = create_matrix(matrix_size)
+m2 = create_matrix(matrix_size)
+print "M1:\n"
+print_matrix(m1, matrix_size)
+print "M2:\n"
+print_matrix(m2, matrix_size)
 
 #envia o tamanho da matriz ao servidor
 sock.send(m)
@@ -60,7 +58,6 @@ file.seek(0,0)
 
 while True:
 	chunk = file.read(1024)
-	print chunk
 	sock.send(chunk)
 	#EOF
 	if chunk == '':
