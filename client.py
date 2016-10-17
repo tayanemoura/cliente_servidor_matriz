fc32d1683a49606d3942605b429b33b33da3ad83
tayanemoura <tayanemoura@id.uff.br>
2016-10-17 11:58:41 -0200

    create_matrix no client

--- client.py
+++ client.py
@@ -1,9 +1,39 @@
 # -*- coding: utf-8 -*-
 import socket
+import random
 import sys
+from datetime import *
 
-def generate_matrix():
-	pass
+a = datetime.now()
+timestamp = a.strftime('%H%M%S%f')
+matrix_file_name = "matriz" + timestamp + ".txt"
+print matrix_file_name
+
+def create_matrix(matrix_size):
+  
+  matrix = []
+  for i in range(matrix_size):
+  	matrix.append([0]*5)
+
+  print matrix
+
+  matrix_file = open(matrix_file_name, 'a+')
+  for i in range(0, matrix_size):
+      for j in range(0, matrix_size):
+          number = random.randint(0, 100)
+          #matrix[i][j] = number
+          print(str(number) + "\t")
+          matrix_file.write((str(number) + "\t"))
+      print()
+      matrix_file.write("\n")
+  print()
+  matrix_file.close()
+  return matrix
+
+def print_matrix(matrix):
+  for i in range(0, matrix_size):
+      for j in range(0, matrix_size):
+          print(str(matrix[i][j]) + "\t")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,14 +46,16 @@ sock.connect(server_addr)
 # tamanho da matriz
 m = sys.argv[1]
 
+m1 = create_matrix(int(m))
+m2 = create_matrix(int(m))
 #print m
 
 #envia o tamanho da matriz ao servidor
 sock.send(m)
 
-file = open('example.txt', 'r')
+file = open(matrix_file_name, 'r')
 
-#file.seek(0,0)
+file.seek(0,0)
 
 while True:
 	chunk = file.read(1024)
--- server.py
+++ server.py
@@ -5,57 +5,41 @@ import thread
 import random
 from datetime import *
 
-matrix_size = 5
-matrix_file_name = "matriz.txt"
-m1, m2
+#matrix_size = 5
 
-def create_matrix():
-  matrix
-  matrix_file = open(matrix_file_name, 'a+')
-  for i in range(0, matrix_size):
-      for j in range(0, matrix_size):
-          number = random.randint(0, 100)
-          matrix[i][j] = number
-          print(str(number) + "\t")
-          matrix_file.write((str(number) + "\t"))
-      print()
-      matrix_file.write("\n")
-  print()
-  matrix_file.close()
-  return matrix
+#matrix_file_name = "matriz.txt" 
 
 def print_matrix(matrix):
   for i in range(0, matrix_size):
       for j in range(0, matrix_size):
           print(str(matrix[i][j]) + "\t")
 
-
 #TODO
 #função para multiplicar as matrizes e salvar em um arquivo multiplica.txt
-def multiply_matrices(m1, m2):
+def multiply_matrices(m1, m2, matrix_size):
 	print "Iniciando multiplicação.."
 	a = datetime.now()
 	#print a.strftime('%Hh%Mm%Ss%fus')
 	#implementação
-  multiply_file = open('multiplica.txt', 'w')
-  matrix_size = len(m1)
+  	multiply_file = open('multiplica.txt', 'w')
+  	matrix_size = len(m1)
 
-  for i in range(matrix_size):
-      for j in range(matrix_size):
-          number = 0
-          for k in range(matrix_size):
-              number += matrix_1[i][k] * matrix_2[k][j]
-          multiply_file.write(str(number) + "\t")
-      if i != matrix_size - 1:
-          multiply_file.write("\n")
+  	for i in range(matrix_size):
+  		for j in range(matrix_size):
+  			number = 0
+        	for k in range(matrix_size):
+        		number += matrix_1[i][k] * matrix_2[k][j]
+          	multiply_file.write(str(number) + "\t")
+      	if i != matrix_size - 1:
+          	multiply_file.write("\n")
 
-  multiply_file.close()
+  	multiply_file.close()
 
 	thread.exit()
 
 #TODO
 #função para somar as matrizes e salvar em um arquivo soma.txt
-def add_matrices(m1, m2):
+def add_matrices(m1, m2, matrix_size):
 	print "Iniciando soma.."
 	a = datetime.now()
 	print a.strftime('%Hh%Mm%Ss%fus')
@@ -76,7 +60,7 @@ def read_client(conn, client):
 		file = file + data
 		if not data:
 			break
-		#print data
+		print data
 		
 	#TODO
 	#separa as duas matrizes - considerando que cada matriz está em cada linha, mas vai depender de como faremos o arquivo 
@@ -89,14 +73,16 @@ def read_client(conn, client):
 	matrix_1 = aux[0]
 	matrix_2 = aux[1]
 
-	thread.start_new_thread(multiply_matrices, tuple([matrix_1,matrix_2]))
-	thread.start_new_thread(add_matrices, tuple([matrix_1,matrix_2]))
+	#thread.start_new_thread(multiply_matrices, tuple([matrix_1,matrix_2]))
+	thread.start_new_thread(add_matrices, tuple([matrix_1,matrix_2, int(m)]))
 
 	#TODO:
 	#ainda precisamos enviar os arquivos multiplica.txt e soma.txt pro usuário
 
 	conn.close()
 	thread.exit()
+
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -107,10 +93,6 @@ sock.bind(server_addr)
 sock.listen(1)
 print "Esperando conexões.."
 
-m1 = create_matrix()
-m2 = create_matrix()
-multiply_matrices(m1,m2)
-print_matrix(m1)
 
 while True:
 	connection, client_addr = sock.accept()
